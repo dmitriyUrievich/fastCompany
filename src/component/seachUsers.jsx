c53490300d093101061c2412b210ebbee167f495
dmitriyUrievich <dmitriy.bubenin@gmail.com>
2022-08-29 18:46:32 +0300

    lesson7.1

---
+++ src/component/seachUsers.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SeachUser = () => {
+  
+  return (
+    <div className="input-group mb-3">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Search..."
+      onChange={}/>
+    </div>
+  )
+}
+
+export default SeachUser
